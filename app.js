a2a5abc916ae573790c4e541b3165cfd94d0a0fa
asbin007 <asbingamer@gmail.com>
2023-03-05 16:10:25 +0545

    starting this project

---
+++ app.js
@@ -0,0 +1,30 @@
+// contact 
+// alert
+ function sendMail(){
+    var params={
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+    const serviceID="service_4rifeoi";
+    const templateID="template_sm09awi";
+     emailjs
+     .send(serviceID,templateID,params)
+     .then((res)=>{
+        document.getElementById("name").value= "";
+        document.getElementById("email").value= "";
+        document.getElementById("message").value= "";
+        console.log(res);
+        alert("Your message is successfull sent");
+
+
+     })
+     .catch((err)=>console.log(err));
+
+
+
+}    
+    
+
+
+
